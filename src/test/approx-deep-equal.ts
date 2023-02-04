70a8c00520ec3dd73cb0198aaf0ec7521ba4be94
Jonny Burger <jonathanburger11@gmail.com>
2023-02-04 16:26:41 +0100

    test suite

---
+++ src/test/approx-deep-equal.ts
@@ -0,0 +1,53 @@
+import assert from "assert";
+
+export function approximately(
+  actual: unknown,
+  expected: unknown,
+
+  delta: number,
+  message: string
+) {
+  if (typeof actual === "number" && typeof expected === "number") {
+    const diff = Math.abs(actual - expected);
+    const larger = Math.max(diff, delta);
+
+    assert.equal(larger, delta, message);
+  } else {
+    assert.equal(actual, expected, message);
+  }
+}
+
+export function deepEqual(
+  actual: object,
+  expected: object,
+  message: string,
+  comparator = assert.equal
+) {
+  for (let key of Object.keys(expected)) {
+    assert.ok(key in actual, message);
+  }
+
+  for (let key of Object.keys(actual)) {
+    assert.ok(key in expected, message);
+
+    // @ts-expect-error
+    if (typeof actual[key] === "object" && typeof expected[key] === "object") {
+      // @ts-expect-error
+      deepEqual(actual[key], expected[key], message, comparator);
+    } else {
+      // @ts-expect-error
+      comparator(actual[key], expected[key], message);
+    }
+  }
+}
+
+export function approxDeepEqual(
+  actual: object,
+  expected: object,
+  delta: number,
+  message: string = "not equal"
+) {
+  deepEqual(actual, expected, message, (a, b) =>
+    approximately(a, b, delta, message)
+  );
+}
